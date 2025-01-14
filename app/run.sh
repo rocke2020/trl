d432d5c92d716fb45c07d75d99e9f1e1f8cc5f83
qichang.dong <dongqichang@ecmas.ai>
2025-01-15 07:10:24 +0800

    u run.sh

--- app/run.sh
+++ app/run.sh
@@ -6,4 +6,19 @@ export CUDA_VISIBLE_DEVICES=$gpu
 # 
 file=app/trl/scripts/dpo.py
 nohup python $file \
+    --dataset_name trl-lib/ultrafeedback_binarized \
+    --model_name_or_path /data/model/Qwen/Qwen2.5-0.5B-Instruct \
+    --learning_rate 5.0e-6 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 8 \
+    --gradient_checkpointing \
+    --logging_steps 25 \
+    --eval_strategy steps \
+    --eval_steps 50 \
+    --output_dir /data/t2s-data/models_verify/Qwen2.5-0.5B-DPO \
+    --no_remove_unused_columns \
+    --use_peft \
+    --lora_r 32 \
+    --lora_alpha 16 \
     > $file-nohup.log 2>&1 &
